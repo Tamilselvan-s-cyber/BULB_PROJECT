a4f527660b57d5128f938bee13d8954cf48227c0
TAMILSELVAN S <tamilsanmugam2005@gmail.com>
2025-03-19 16:21:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var on_btn = document.getElementById("on");
+var off_btn = document.getElementById("off");
+var img = document.getElementById("bulb");
+
+
+function bulb_on()
+{
+    img.src="https://5.imimg.com/data5/WL/SG/MY-8835990/10w-gls-light-bulb.jpg"
+}
+function bulb_off()
+{
+    img.src="https://www.citypng.com/public/uploads/preview/transparent-hd-real-light-bulb-704081695125968fldw2x56hs.png"
+}
